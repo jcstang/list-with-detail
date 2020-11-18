032d74c6e895d4de9f36f898e34e795530c51190
Jacob Stanger <devjake@outlook.com>
2020-11-17 21:25:30 -0700

    add reptileView

--- list-with-detail/ReptilesView.swift
+++ list-with-detail/ReptilesView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ReptilesView.swift
 //  list-with-detail
 //
 //  Created by Jacob Stanger on 11/17/20.
@@ -7,46 +7,13 @@
 
 import SwiftUI
 
-struct ReptileCircleView: View {
-    let Reptile: Reptile
-    
-    var body: some View {
-        ZStack {
-            Image(Reptile.icon)
-                .shadow(radius: 3)
-                .frame(width: 100, height: 100, alignment: .center)
-                .overlay(
-                    Circle().stroke(Color.purple, lineWidth: 3)
-                )
-                .clipShape(Circle())
-            //            Text(Reptile.icon)
-            //                .shadow(radius: 3)
-            //                .font(.largeTitle)
-            //                .frame(width: 65, height: 65)
-            //                .overlay(
-            //                    Circle().stroke(Color.purple, lineWidth: 3)
-            //                )
-        }
-    }
-}
-
-
-struct Reptile: Identifiable {
-    let id = UUID()
-    let name: String
-    let icon: String
-    let description: String
-}
-
-struct ContentView: View {
-    @State private var showingSheet = false
-    @State private var sort: Int = 0
-    
+struct ReptilesView: View {
     private let repList: [Reptile] = [
         Reptile(name: "gimli", icon: "default_reptile", description: "mojave ball"),
         Reptile(name: "Zelda", icon: "default_reptile", description: "hognose xanth"),
         Reptile(name: "Erso", icon: "default_reptile", description: "cornsnake")
     ]
+    
     var body: some View {
         VStack {
             NavigationView {
@@ -71,8 +38,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ReptilesView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ReptilesView()
     }
 }
